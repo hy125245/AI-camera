4701efb39ce1bce36623d049de979e190e0741a2
hy125245 <hy125245@163.com>
2024-07-11 16:08:23 +0800

    更新：初步设定CMake文件

---
+++ build/CMakeFiles/AI-camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/AI-camera"
+  "../bin/AI-camera.pdb"
+  "CMakeFiles/AI-camera.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AI-camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/AI-carmera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "AI-carmera"
+  "AI-carmera.pdb"
+  "CMakeFiles/AI-carmera.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AI-carmera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
