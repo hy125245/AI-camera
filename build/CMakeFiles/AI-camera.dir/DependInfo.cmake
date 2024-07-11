984133571993f4990493a438587b3c93d890c520
hy125245 <hy125245@163.com>
2024-07-11 16:40:19 +0800

    新增：语音识别功能

--- build/CMakeFiles/AI-camera.dir/DependInfo.cmake
+++ build/CMakeFiles/AI-camera.dir/DependInfo.cmake
@@ -1,8 +1,23 @@
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "C"
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/hy125245/AI-camera/src/linuxrec.c" "/home/hy125245/AI-camera/build/CMakeFiles/AI-camera.dir/src/linuxrec.c.o"
+  "/home/hy125245/AI-camera/src/speech_recognizer.c" "/home/hy125245/AI-camera/build/CMakeFiles/AI-camera.dir/src/speech_recognizer.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include/voice"
+  "../src"
+  "../include"
+  "../("
+  "../)"
+  )
 set(CMAKE_DEPENDS_CHECK_CXX
   "/home/hy125245/AI-camera/main.cpp" "/home/hy125245/AI-camera/build/CMakeFiles/AI-camera.dir/main.cpp.o"
   )
@@ -10,7 +25,11 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include/voice"
+  "../src"
   "../include"
+  "../("
+  "../)"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/AI-camera.dir/cmake_clean.cmake
+++ build/CMakeFiles/AI-camera.dir/cmake_clean.cmake
@@ -2,9 +2,11 @@ file(REMOVE_RECURSE
   "../bin/AI-camera"
   "../bin/AI-camera.pdb"
   "CMakeFiles/AI-camera.dir/main.cpp.o"
+  "CMakeFiles/AI-camera.dir/src/linuxrec.c.o"
+  "CMakeFiles/AI-camera.dir/src/speech_recognizer.c.o"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/AI-camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
